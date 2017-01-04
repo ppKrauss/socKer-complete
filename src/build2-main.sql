c46c473ff8f0b7aeaccd7485926e99cedd38d0c9
Peter <ppkrauss@gmail.com>
2017-01-04 16:17:33 -0200

    Update build2-main.sql

--- src/build2-main.sql
+++ src/build2-main.sql
@@ -14,7 +14,35 @@ CREATE TABLE socker.agent (
 	status smallint DEFAULT  '001'::bit(3)::int CHECK(socker.valid_enum(status,'status-type')),
      -- STATUS CONVENTION: bit3=not/endorsed, bit1=informal/formal, bit0=inactive/ative.
 	info JSONb NOT NULL CHECK (trim(info->>'name_main')>''),
-	kx_name text NOT NULL CHECK(char_length(name)<300),  -- std(info). Local name.
-	kx_urn text NOT NULL CHECK(char_length(urn)<500),    -- std(info). <=Like URN LEX.
+	kx_name text NOT NULL CHECK(char_length(name)<300),  -- cache from info, local name.
+	kx_urn text NOT NULL CHECK(char_length(urn)<500),    -- cache from info, like URN LEX.
 	UNIQUE(kx_urn)
 );    -- need final check for info
+
+CREATE TABLE socker.contacthing (
+	--
+	-- See UML, ContactPoint and https://schema.org/Thing
+	--
+	thid serial NOT NULL PRIMARY KEY,
+	thtype int NOT NULL CHECK(socker.valid_enum(thtype,'thtype')), -- vals 1=telephon, 2=email, etc.
+	needcomplement boolean NOT NULL DEFAULT false,
+	kx_urn text NOT NULL CHECK(char_length(urn)<500),              -- cache from info
+	info JSONb NOT NULL CHECK (trim(info->>'val_main')>''),
+	UNIQUE(kx_urn)
+);    -- need final check for info
+
+CREATE TABLE socker.contactpoint (
+	--
+	-- See UML, ContactPoint and https://schema.org/Thing
+	--
+	id serial NOT NULL PRIMARY KEY,
+	agid bigint NOT NULL REFERENCES socker.agent(agid),
+	thid bigint NOT NULL REFERENCES socker.telcom(thid),
+	isowner boolean,                     	-- null=no information, true=is owner, false=is not. 
+	ismain boolean NOT NULL DEFAULT false,
+	rule int NOT NULL DEFAULT 0 CHECK(socker.valid_enum(rule,'ctrule')), -- undef, home, work, corresp, etc.
+	kx_complement text NOT NULL DEFAULT '',
+	info JSONb CHECK (trim(info->>'complement')>''),
+	UNIQUE(agid,thid,kx_complement)
+);    -- need final check for info
+
